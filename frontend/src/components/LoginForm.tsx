f7bc2b69ef03bff3cbf38def3ce2729e72eaac75
angelofonseca <angeloniemeyer97@gmail.com>
2025-10-06 08:45:05 -0300

    Adjust login form

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -3,9 +3,9 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { adminLogin, login } from "@/services/api";
+import { login } from "@/services/api";
 import { useAuthContext } from "@/context/AuthContext";
 
 export function LoginForm({
@@ -19,7 +19,6 @@ export function LoginForm({
     password: "",
   });
   const { setLocalLogin } = useAuthContext();
-  const location = useLocation();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { target } = event;
@@ -28,10 +27,7 @@ export function LoginForm({
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const data =
-      location.pathname === "/login"
-        ? await login(form)
-        : await adminLogin(form);
+    const data = await login(form);
     if (!data.token) {
       setError("E-mail e/ou senha inválido(s).");
       return;
